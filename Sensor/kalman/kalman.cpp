85dfb3d1774405c3d3ad6f461102a9e524414604
Armon Hakimi <armonah2@gmail.com>
2020-01-26 14:40:55 +0100

    added basic functions. need to calculate state space matrices and states.

--- Sensor/kalman/kalman.cpp
+++ Sensor/kalman/kalman.cpp
@@ -1,1 +1,35 @@
 #include "kalman.h"
+#include "BasicLinearAlgebra/BasicLinearAlgebra.h"
+using namespace BLA;
+
+void Kalman::updateKalmanGain(){
+    K = P_bar*(~C) * (( C*P_bar*(~C) + R ).Inverse());
+}
+
+void Kalman::updateXHat(float measurement){
+    if (measurement){
+        x_hat = x_bar + K*( measurement - C*x_bar );
+    }else{
+        x_hat = x_bar;  //if no new measurement available, predicted x = estimated x.
+    }
+}
+
+void Kalman::updatePMatrix(float measurement){
+    if (measurement){
+        P_hat = (I - K*C)*P_bar*(~(I-K*C)) + K*R*(~K);
+    }else{
+        P_hat = P_bar;  //if no new measurement available, predicted P = estimated P.
+    }
+}
+
+void Kalman::updateXBar(float input){
+    x_bar = A*x_hat + B*input;
+}
+
+void Kalman::updatePBar(){
+    P_bar = A*P_hat*(~A) + Q;
+}
+
+Matrix<2,1> Kalman::getStates(){
+    return x_hat;
+}
--- Sensor/kalman/kalman.h
+++ Sensor/kalman/kalman.h
@@ -2,10 +2,32 @@
 using namespace BLA;
 
 class Kalman {
-private:
-    //matrices
+private:                    //dimensions below are temporary, need to be double-checked. initial values must also be found.
+    Matrix<2,2> A;
+    Matrix<2,1> B;
+    const Matrix<1,2> C = ???;
+    const float R = ???;            //covariance (uncertainty) in measurement; tuning variable.
+    const Matrix<2,2> Q = ???;      //covariance in disturbance; tuning var.
+    const Matrix<2,2> I = {1, 0, 0, 1};
+
+    Matrix<2,1> K;      //Kalman gain
+    Matrix<2,2> P_hat;  //a posteriori
+    Matrix<2,2> P_bar;  //a priori
+    Matrix<2,1> x_hat;
+    Matrix<2,1> x_bar;  //note that x_bar = x_hat_bar = a priori
+
     
 public:
-    Kalman();
-    float calculateKalman();
+    Kalman(Matrix<2,2> A1, Matrix<2,1> B1, Matrix<2,2> P1, Matrix<2,1> x1){A = A1; B = B1; P_bar = P1; x_bar = x1;}
+
+    //Correcting with new data
+    void updateKalmanGain();
+    void updateXHat(float measurement);
+    void updatePMatrix(float measurement);
+
+    //Predicting ahead
+    void updateXBar(float input);
+    void updatePBar();
+ 
+    Matrix<2,1> getStates();
 };
